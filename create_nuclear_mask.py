784029dd3e07d6001191db41392c870a9cc5ee45
sayantaneebiswas <sayantaneebiswas@gmail.com>
2021-10-02 14:04:14 -0400

    Created mask array for gaussian filter

---
+++ create_nuclear_mask.py
@@ -0,0 +1,45 @@
+import scipy.ndimage
+def create_nuclear_mask(txyz_coords,resolution,shape,gaussian_size):
+    ''' Create a gaussian mask around each txyz coordinate in the dataset
+    Inputs: 
+        txyz_coords: list(list(int))
+            A list of coordinates (t, x, y, z)
+
+        resolution: list(int)
+            Approximate pixel resolution in (t, x, y, z) (where t is 1)
+
+        shape: list(int)
+            Total shape of the dataset (t, x, y, z) - number of pixels in each dimension
+
+        gaussian_size: float
+            Parameter to determine the size of the gaussian mask
+
+    Returns:
+        An array of the same shape as the data, with data type float, with gaussian blobs at each coordinate.
+    '''
+    # step 0: create a np array of zeros (of the shape of the mask)
+
+    # step 1: create a binary mask with a 1 at every coordinate
+
+    # step 2: Feed that mask into scipy.ndimage.gaussian_filter()
+
+    mask_arr = np.zeros(shape)
+    for co in txyz_coords:
+        t = co[0]
+        x = co[1]
+        y = co[2]
+        z = co[3]
+        mask_arr[t][x][y][z] = 1
+
+    mask_gaussian = scipy.ndimage.gaussian_filter(mask_arr, sigma=gaussian_size)
+
+
+
+
+
+
+
+
+
+
+
